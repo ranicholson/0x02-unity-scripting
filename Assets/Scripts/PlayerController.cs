013b08edf5b4c72b166e24266701b29612175c3f
Allen Nicholson <66395341+ranicholson@users.noreply.github.com>
2021-08-04 14:19:49 -0500

    Task 4-9 updates

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
+    private int score = 0;
+
     public float speed = 5f;
+    public int health = 5;
     public Rigidbody rb;
 
 
@@ -18,6 +22,29 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (health == 0)
+        {
+            Debug.Log("Game Over!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Pickup")
+        {
+            score++;
+            Debug.Log ("Score: " + score);
+            Object.Destroy(other.gameObject);
+        }
+
+        if (other.tag == "Trap")
+        {
+            health--;
+            Debug.Log("Health: " + health);
+        }
+
+        if (other.tag == "Goal")
+            Debug.Log("You win!");
     }
 }
